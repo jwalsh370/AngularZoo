08aef48c8fc846c6e71f7c92dba142292818ed1c
jwalsh370 <jwalsh370@gmail.com>
2017-05-05 18:11:26 -0700

    add animal profile info to app.

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'app-root',
+  template: ``
+})
+
+export class AppComponent{
+  masterZooList:Zoo[]=[
+    new Zoo("Boo", "Polar Bear", 7,"Icy Seals", "Artic Sea", 6, "Male", "Ice showers", "Saunas", "http://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Eisbär_1996-07-23.jpg/512px-Eisbär_1996-07-23.jpg"),
+    new Zoo("Tata", "Tiger", 3,"Steak", "India", 2, "Female", "Climbing Trees", "Monkeys", "http://defenders.org/sites/default/files/styles/large/public/tiger-dirk-freder-isp.jpg"),
+    new Zoo("Balto", "Wolf", 19,"Sheep", "Alaska", 9, "Male", "Alone Time", "Packs", "http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/gray-wolf-closeup.jpg"),
+    new Zoo("Rico", "Komodo Dragon", 7,"Water Buffalo", "Komodo", 2, "Male", "Sun Bathing", "People", "https://img.purch.com/rc/300x200/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzAzNy8wOTUvb3JpZ2luYWwva29tb2RvLWRyYWdvbi5qcGc="),
+    new Zoo("Sasha", "Anaconda", 4,"capyabaras", "South America", 5, "Female", "Sun Shine", "Florida", "http://yourshot.nationalgeographic.com/u/ss/fQYSUbVfts-T7pS2VP2wnKyN8wxywmXtY0-FwsgxpiuXsl7pJFUA_afdrSxZN9COza5Od1_RwTq89EHGSWlW/"),
+    new Zoo("California", "Lion", 14,"Zebra", "Tanzania", 1, "Female", "Sleeping", "Loud Noise", "http://cdn.c.photoshelter.com/img-get/I0000kZAjh5mwpAk/s/870/870/This-is-Namibia-No-11-Just-Stare.jpg"),
+
+  ];
---
+++ app/zoo.model.ts
@@ -0,0 +1,3 @@
+export class Keg{
+  constructor (public name: string, public species: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string,public likes: string, public dislikes: string, public image: string){}
+}
