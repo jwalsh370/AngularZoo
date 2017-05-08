4a0d4517a466c2d38394ab71900dc4da4812c38b
jwalsh370 <jwalsh370@gmail.com>
2017-05-08 08:17:28 -0700

    remove nav bar.

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,23 +13,7 @@ import { Zoo } from './zoo.model';
       </div>
     </div>
   </div>
-  <div id="menuDiv">
-    <nav id="menu">
-       <ul>
-        <li class="rocket"><a href=""></a></li>
-        <li class="wine"><a href="">On Tap</a></li>
-        <li class="burger"><a href="">Good Eats</a></li>
-        <li class="comment"><a href="">Contact</a></li>
-        <li class="sport" ><a href="">Activities</a></li>
-        <li class="earth"><a href="">Locations</a></li>
-        <div class="current">
-          <div class="top-arrow"></div>
-          <div class="current-back"></div>
-          <div class="bottom-arrow"></div>
-        </div>
-      </ul>
-    </nav>
-  </div>
+
   <div class="container">
     <br><zoo-list [childZooList] = "masterZooList" (clickSender)="editZoo($event)"></zoo-list>
     <div class="row">
@@ -45,7 +29,7 @@ import { Zoo } from './zoo.model';
 
 export class AppComponent{
   masterZooList:Zoo[]=[
-    new Zoo("Boo", "Mammal", 7,"Icy Seals", "Artic Sea", 6, "Male", "Ice showers", "Saunas", "http://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Eisbär_1996-07-23.jpg/512px-Eisbär_1996-07-23.jpg"),
+    new Zoo("Boo", "Mammal", 7,"Icy Seals", "Artic Sea", 6, "Male", "Ice showers", "Saunas", "http://www.typosthes.gr/files/temp//E81D65815E94DE1DD641C284480350E4.jpg"),
     new Zoo("Tata", "Mammal", 3,"Steak", "India", 2, "Female", "Climbing Trees", "Monkeys", "http://defenders.org/sites/default/files/styles/large/public/tiger-dirk-freder-isp.jpg"),
     new Zoo("Balto", "Mammal", 19,"Sheep", "Alaska", 9, "Male", "Alone Time", "Packs", "http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/A-G/gray-wolf-closeup.jpg"),
     new Zoo("Rico", "Reptile", 7,"Water Buffalo", "Komodo", 2, "Male", "Sun Bathing", "People", "https://img.purch.com/rc/300x200/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzAzNy8wOTUvb3JpZ2luYWwva29tb2RvLWRyYWdvbi5qcGc="),
